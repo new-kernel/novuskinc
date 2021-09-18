3c8262c2cb72e78247c46665d740d119ec0228a8
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-09-18 11:32:36 -0400

    Added Aarch64 and arm32 sysnumbers.

--- systbl.rs
+++ systbl.rs
@@ -6,3 +6,21 @@ cfg_if! {
         pub const REBOOT: i32 = 3;
     }
 }
+
+cfg_if! {
+    if #[cfg(target_arch = "aarch64")] {
+        pub const VERSION: i32 = 1;
+        pub const READ: i32 = 63;
+        pub const WRITE: i32 = 64;
+        pub const REBOOT: i32 = 142;
+    }
+}
+
+cfg_if! {
+    if #[cfg(target_arch = "arm")] {
+        pub const READ: i32 = 3;
+        pub const WRITE: i32 = 4;
+        pub const REBOOT: i32 = 88;
+        pub const VERSION: i32 = 2;
+    }
+}
