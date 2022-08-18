68d9573b5e7d1f563ed22c194e4b9f25339891ce
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2022-08-18 14:04:10 -0400

    Added functions to start the arch and main kernel.

--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -1,2 +1,19 @@
 pub mod macros;
 pub mod types;
+
+extern "C" {
+    /// ``arch_prepare_init`` uses the architecture specific kernel to get ready for the main kernel
+    /// initialization
+    pub fn arch_prepare_init();
+
+    /// This function is used to setup the architecture specific kernel
+    pub fn setup_arch();
+
+    /// Starts the architecture and main kernel, used for both the main Novusk kernel and a device
+    /// specific kernels
+    pub fn start_kernel();
+
+    /// ``kernel_init`` is the kernel's "main" function, it initializes all non architecture
+    /// specific functions
+    pub fn kernel_init();
+}
